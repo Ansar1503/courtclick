0f4f2cdb5168ff7793acb9dad816a527dc2b2ab1
Ansar1503 <muhammedansarma1503@gmail.com>
2026-07-08 21:16:54 +0530

    ant design package

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,24 @@
 import Image from "next/image";
+import navLogo from "@/public/navbarLogo.svg"
+
+const navLinks = [
+  
+]
 
 export default function Home() {
   return (
     <div>
       <nav>
-        <div>
+        <div >
           <div>
-
+            <Image
+              src={navLogo}
+              alt="Court Click Logo"
+              width={100}
+              height={60}
+            />
           </div>
+          
 
         </div>
         <div>
